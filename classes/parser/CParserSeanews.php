ae514e5d2cc3fce7d8a6428257b33f06d6d36da8
Alexey Storozhev <dark.storoj@gmail.com>
2012-12-07 03:57:02 +0400

    seanews.ru parser implemented

---
+++ classes/parser/CParserSeanews.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: storoj
+ * Date: 07.12.12
+ * Time: 3:31
+ */
+
+class CParserSeanews extends CAParser {
+
+    public function sourceID()
+    {
+        return 2;
+    }
+
+    private function extractTime($str)
+    {
+        $months = array(
+            'января', 'февраля', 'марта',
+            'апреля', 'мая', 'июня', 'июля',
+            'августа', 'сентября', 'октября',
+            'ноября', 'декабря'
+        );
+        $pattern = '#\d+ ([^\d]+) \d+, [0-9:]+#';
+        preg_match($pattern, $str, $match);
+
+        $monthName = $match[1];
+        $monthIndex = array_search($monthName, $months) + 1;
+
+        $dateStr = str_replace(' '.$monthName.' ', '.'.$monthIndex.'.', $match[0]);
+
+        $time = DateTime::createFromFormat('d.m.Y, H:i', $dateStr);
+
+        if ($time) {
+            return $time->getTimestamp();
+        }
+        return false;
+    }
+
+    public function extractDataFromHTML($html)
+    {
+        $document = phpQuery::newDocumentHTML($html);
+        $articleElement = $document->find('#forprint .text');
+
+        $title = $articleElement->find('h3')->text();
+        $time = $articleElement->find('.timelabel')->text();
+        $time = $this->extractTime($time);
+
+        $articleElement->find('h3')->remove();
+        $articleElement->find('.timelabel')->remove();
+
+        $text = substr($articleElement->text(), 0, 50);
+
+        $result = array(
+            'title'	=> trim($title),
+            'time'	=> trim($time),
+            'text'	=> trim($text)
+        );
+
+        return $result;
+    }
+
+    public function extractURLListFromHTML($html)
+    {
+        $pattern = '#newsID=(\d+)#';
+        preg_match_all($pattern, $html, $matches);
+
+        $result = array();
+        foreach ($matches[1] as $articleID) {
+            $result[] = $this->webSiteBaseURL().'/news/news.asp?newsID='.$articleID;
+        }
+
+        return $result;
+    }
+
+    public function webSiteBaseURL()
+    {
+        return 'http://www.seanews.ru';
+    }
+
+    public function getNewsListPageContents($page)
+    {
+        $url = $this->webSiteBaseURL().'/news/Default.asp?kw=&geo=&pg='.$page;
+
+        return Downloader::defaultDownloaderForURL($url)->download();
+    }
+}

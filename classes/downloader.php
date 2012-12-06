b46a50e700695e20f4cf0df0e3333d7032978f7e
Alexey Storozhev <dark.storoj@gmail.com>
2012-12-07 03:27:44 +0400

    cacheable downloader

--- classes/downloader.php
+++ classes/downloader.php
@@ -13,6 +13,7 @@ class Downloader {
         CURLOPT_FOLLOWLOCATION  => true,
         CURLOPT_CONNECTTIMEOUT  => 10
     );
+    public $useCache = true;
 
     public function __construct($url = false, $options = array())
     {
@@ -28,15 +29,46 @@ class Downloader {
         $this->options = array_merge_numeric($defaultOptions, $options);
     }
 
+    public static function defaultDownloaderForURL($url)
+    {
+        return new Downloader($url);
+    }
+
+    public function getCacheFileNameForURL($url)
+    {
+        $cacheDir = PATH_ROOT.'cache/';
+
+        $urlParts = parse_url($url);
+        $host = $urlParts['host'];
+        $path = preg_replace('#/$#', '', $urlParts['path']);
+
+        return $cacheDir.$host.$path.'.html';
+    }
+
     public function download()
     {
+        $returnTransfer = isset($this->options[CURLOPT_RETURNTRANSFER]) && $this->options[CURLOPT_RETURNTRANSFER];
+        if ($returnTransfer) {
+            $cacheFileName = $this->getCacheFileNameForURL($this->url);
+            if (file_exists($cacheFileName)) {
+                return file_get_contents($cacheFileName);
+            }
+        }
+
         $options = array_merge_numeric($this->options, array(
             CURLOPT_URL     => $this->url
         ));
 
         $ch = curl_init();
         curl_setopt_array($ch, $options);
-        return curl_exec($ch);
+        $result = curl_exec($ch);
+
+        if ($returnTransfer) {
+            @mkdir(dirname($cacheFileName), 0777, true);
+            file_put_contents($cacheFileName, $result);
+        }
+
+        return $result;
     }
 
 }

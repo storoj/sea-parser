362c193a5699d4adb653e1a04a554c8540819c34
Alexey Storozhev <dark.storoj@gmail.com>
2013-01-23 00:50:03 +0400

    added refreshing request for phrases groups

--- js/main.js
+++ js/main.js
@@ -6,46 +6,75 @@
  * To change this template use File | Settings | File Templates.
  */
 
-$(function(){
-    $('.datepicker').datepicker({language: 'ru'});
+function addGroupTab(tabID, title, phrases)
+{
+    $('#settingsTabs').append('<li><a href="#settings-'+tabID+'" data-tab-id="'+tabID+'" data-toggle="tab">'+title+'</a></li>');
+    $('#settings .tab-content').append('<div class="tab-pane" id="settings-'+tabID+'"><textarea name="phrases['+tabID+']">'+phrases+'</textarea></div>');
+}
 
-    function addGroupTab(tabID, title, phrases)
-    {
-        $('#settingsTabs').append('<li><a href="#settings-'+tabID+'" data-tab-id="'+tabID+'" data-toggle="tab">'+title+'</a></li>');
-        $('#settings .tab-content').append('<div class="tab-pane" id="settings-'+tabID+'"><textarea name="phrases['+tabID+']">'+phrases+'</textarea></div>');
-    }
+function activateFirstGroupTab()
+{
+    $('#settingsTabs li:first a').tab('show');
+}
 
-    function activateFirstGroupTab()
-    {
-        $('#settingsTabs li:first a').tab('show');
-    }
+function activateLastGroupTab()
+{
+    $('#settingsTabs li:last a').tab('show');
+}
 
-    function activateLastGroupTab()
-    {
-        $('#settingsTabs li:last a').tab('show');
-    }
+var settingsGetQuery = new AjaxQuery({
+    url: '/settings/phrases',
+    autoexecute: true,
+    callbacks: {
+        success: function(status, response) {
+            if (status != 'ok') {
+                alertbox(response.message, status);
+            } else {
+                for (var groupDataIndex in response.result) {
 
-    var settingsGetQuery = new AjaxQuery({
-        url: '/settings/phrases',
-        callbacks: {
-            success: function(status, response) {
-                if (status != 'ok') {
-                    alertbox(response.message, status);
-                } else {
-                    for (var groupDataIndex in response.result) {
+                    var groupData = response.result[groupDataIndex];
+                    var groupName = groupData.name;
+                    var phrasesText = groupData.phrases.join("\n");
 
-                        var groupData = response.result[groupDataIndex];
-                        var groupName = groupData.name;
-                        var phrasesText = groupData.phrases.join("\n");
-
-                        addGroupTab(groupDataIndex-0+1, groupName, phrasesText);
-                    }
-                    activateFirstGroupTab();
+                    addGroupTab(groupDataIndex-0+1, groupName, phrasesText);
                 }
+                activateFirstGroupTab();
             }
         }
-    });
-    settingsGetQuery.execute();
+    }
+});
+
+var settingsSaveQuery = new AjaxQuery({
+    url: "/settings/phrases/save",
+    callbacks: {
+        success: function(status, response) {
+            alertbox(response.message, status);
+        }
+    }
+}, {});
+
+var phrasesGroupsQuery = new AjaxQuery({
+    url: '/settings/phrases/groups',
+    autoexecute: true,
+    callbacks: {
+        success: function(status, response) {
+            var content = '';
+            var phrasesGroupsInfo = response.result;
+            for(var i=0; i<phrasesGroupsInfo.length; i++){
+                var item = phrasesGroupsInfo[i];
+                content += '<label class="checkbox"><input type="checkbox" name="phrasesGroups[]" value="'+item.id+'" checked>'+item.name+'</label>';
+            }
+            $('#phraseGroupsList').html(content);
+        }
+    }
+});
+
+ajaxHandler.addQuery(settingsGetQuery, 'getSettings');
+ajaxHandler.addQuery(settingsSaveQuery, 'saveSettings');
+ajaxHandler.addQuery(phrasesGroupsQuery, 'phrasesGroups');
+
+$(function(){
+    $('.datepicker').datepicker({language: 'ru'});
 
     $('#settings form').submit(function(){
         var keywordsGroupsData = [];
@@ -58,15 +87,10 @@ $(function(){
             keywordsGroupsData.push({name: groupName, phrases: phrases});
         });
 
-        var settingsSaveQuery = new AjaxQuery({
-            url: "/settings/phrases/save",
-            callbacks: {
-                success: function(status, response) {
-                    alertbox(response.message, status);
-                }
-            }
-        }, {groups: keywordsGroupsData});
-        settingsSaveQuery.execute();
+        ajaxHandler
+            .getQuery('saveSettings')
+            .setData({groups: keywordsGroupsData})
+            .execute();
 
         return false;
     });
@@ -95,18 +119,11 @@ $(function(){
         activateFirstGroupTab();
     });
     
-    var phrasesGroupsQuery = new AjaxQuery({
-        url: '/settings/phrases/groups',
-        callbacks: {
-            success: function(status, response){
-
-            }
-        }
-    });
-    ajaxHandler.addQuery(phrasesGroupsQuery, 'phrasesGroups');
-    
     $('#phrasesGroupsRefreshButton').click(function(){
         ajaxHandler.getQuery('phrasesGroups').execute();
         return false;
     });
+
+
+    ajaxHandler.getAutoExecQueries().execute();
 });
